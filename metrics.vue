7f6229c493f3a544d52839269940ae6228b80187
Sean Moran <sean_r_moran@msn.com>
2021-07-10 17:16:49 -0400

    Retrieved the metrics in preparation for displaying them on the page

--- metrics.vue
+++ metrics.vue
@@ -1,12 +1,26 @@
 <template>
-  <div class="example">{{ msg }}</div>
 </template>
 
 <script>
+  const metricsUrl = 'http://planogram.stevensoncompany.com/api/metrics/store';
+
+  import axios from 'axios';
+
   export default {
-    data() {
-      return {msg: 'Hello world!'};
+    async beforeCreate() {
+      let results;
+      try {
+        ({data: {data: results}} = await axios(metricsUrl));
+      } catch {}
+
+      this.results = parseMetrics(results);
     },
+
+    data: () => ({results: []}),
+  };
+
+  function parseMetrics(resp) {
+    return resp;
   }
 </script>
 
